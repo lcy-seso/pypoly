206cd6093252544307726495f2ad63684d101613
lcy-seso <lcy.seso@gmail.com>
2020-04-17 11:25:41 +0800

    some experiments.

--- pypet/__init__.py
+++ pypet/__init__.py
@@ -1,21 +1,30 @@
+import pdb
+
 import __future__
 
 import torch
+
+from torch.jit._recursive import concrete_type_store
 
 from .cells import *
 from .tensor_array import TensorArray
 
 
-def scop(obj):
+def scop(nn_module):
     r"""parse static control parts.
     """
-    if isinstance(obj, torch.nn.Module):
-        if not hasattr(obj, 'forward'):
+    if isinstance(nn_module, torch.nn.Module):
+        if not hasattr(nn_module, 'forward'):
             raise Exception('forward implementation is not found.')
-        method = getattr(obj, 'forward', None)
+        method = getattr(nn_module, 'forward', None)
 
         # TODO(Ying) recursive parsing is required.
-        return torch.jit.get_jit_def(method)
+        concrete_type = concrete_type_store.get_or_create_concrete_type(
+            nn_module)
+        methods = torch.jit._recursive.infer_methods_to_compile(nn_module)
+        for method in methods:
+            print(method.def_)
+
     else:
         raise NotImplementedError(
             'parse codes other than nn.Module is not implemented yet.')
--- pypet/examples/test1.py
+++ pypet/examples/test1.py
@@ -1,3 +1,5 @@
+import pdb
+
 import os
 import sys
 sys.path.insert(
@@ -18,20 +20,27 @@ from pypet import TensorArray
 
 
 class MyModule(nn.Module):
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, cells):
         super(MyModule, self).__init__()
 
         self.register_buffer('init_state', torch.zeros((1, hidden_size)))
 
+        self.cell1 = cells[0]
+        self.cell2 = cells[1]
+        self.cell3 = cells[2]
+
+        self.cells = [self.cell1, self.cell2, self.cell3]
+
     def forward(self, input: List[List[Tensor]], batch_size: int,
-                seq_lens: List[int], cells: List[nn.Module], depth: int,
+                seq_lens: List[int], depth: int,
                 output_size: int) -> TensorArray:
 
         # Declare array shape.
-        output = TensorArray(batch_size, seq_lens, depth, output_size)
+        output: TensorArray = TensorArray(batch_size, seq_lens, depth,
+                                          output_size)
 
         for i in range(batch_size):
-            seq_len = seq_lens[i]
+            seq_len: Tensor = seq_lens[i]
             for j in range(seq_len):  # data-dependent loop bound.
                 for k in range(depth):
                     if j == 0:
@@ -44,7 +53,7 @@ class MyModule(nn.Module):
                     else:
                         x = output.read(i, j, k - 1)
 
-                    h = cells[k](x, h_prev)
+                    h = self.cells[k](x, h_prev)
                     output.write(h, i, j, k)
 
 
@@ -76,9 +85,9 @@ if __name__ == '__main__':
 
     outputs = []
 
-    m = MyModule(hidden_size).to(device)
+    m = MyModule(hidden_size, cells).to(device)
     parsed = pypet.scop(m)
-    print(parsed)
+    # print(parsed)
 
     # This example cannot be run now, because of lacking necessary
     # implementations. Do not uncomment the below line.
