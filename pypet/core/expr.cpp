cab34ac50a9f42d0887269d8d82ab43ac9478dc7
lcy-seso <lcy.seso@gmail.com>
2020-05-12 15:49:55 +0800

    fix style.

--- pypet/core/expr.cpp
+++ pypet/core/expr.cpp
@@ -3,8 +3,27 @@
 namespace pypet {
 
 __isl_null PypetExpr* PypetExprFree(__isl_take PypetExpr* expr) {
-  // TODO(Ying): placeholder, not implemented yet.
-  return nullptr;
-}
+  if (!expr) return nullptr;
+  if (--expr->ref > 0) return nullptr;
 
+  for (unsigned int i = 0; i < expr->arg_num; ++i) PypetExprFree(expr->args[i]);
+  free(expr->args);
+
+  switch (expr->type) {
+    case PYPET_EXPR_ACCESS:
+      isl_id_free(expr->acc.ref_id);
+      for (int type = PYPET_EXPR_ACCESS_BEGIN; type < PYPET_EXPR_ACCESS_END;
+           ++type)
+        isl_union_map_free(expr->acc.access[type]);
+      isl_multi_pw_aff_free(expr->acc.index);
+      break;
+    case PYPET_EXPR_CALL:
+      // TODO(Ying): Not implmented yet.
+      break;
+    case PYPET_EXPR_OP:
+    case PYPET_EXPR_ERROR:
+      break;
+      return nullptr;
+  }
+}
 }  // namespace pypet
